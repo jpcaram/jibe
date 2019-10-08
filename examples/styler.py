592ddfc289f45a97ec1b6398f805ddbf25685479
Juan Pablo Caram <jpcaram@gmail.com>
2019-10-07 21:33:44 -0400

    Examples.

---
+++ examples/styler.py
@@ -0,0 +1,64 @@
+import tornado.ioloop
+from webpy import MainApp
+from webpy import Widget, Button, Input, HBox, VBox, CheckBox, Label
+
+
+class StylerApp(MainApp):
+
+    def __init__(self):
+        super().__init__()
+
+        topbox = VBox()
+        self.button_black = Button("Black", style={'margin': '3px'})
+        self.button_red = Button("Red", style={'margin': '3px'})
+        self.button_large = Button("Large", style={'margin': '3px'})
+        self.button_small = Button("Small", style={'margin': '3px'})
+        self.buttonsbox = HBox()
+        self.buttonsbox.children = [
+            self.button_black,
+            self.button_red,
+            self.button_large,
+            self.button_small
+        ]
+
+        self.labelA = Label("Label A")
+
+        self.secbox = HBox()
+
+        self.secbox.children = [
+            self.labelA
+        ]
+
+        topbox.children = [
+            self.buttonsbox, self.secbox
+        ]
+
+        self.button_black.register('click', self.on_click)
+        self.button_red.register('click', self.on_click)
+        self.button_large.register('click', self.on_click)
+        self.button_small.register('click', self.on_click)
+
+        self.children = [
+            topbox
+        ]
+
+    def on_click(self, source):
+        print(f'Style Swapper App on_click(): {source.label}')
+
+        if source.label.lower() == "black":
+            self.labelA.css({'color': 'black'})
+        elif source.label.lower() == "red":
+            self.labelA.css({'color': 'red'})
+        elif source.label.lower() == "large":
+            self.labelA.css({'font-size': '20px'})
+        elif source.label.lower() == "small":
+            self.labelA.css({'font-size': '12px'})
+        else:
+            print("NO SUCH BUTTON!")
+
+
+if __name__ == "__main__":
+    # app = MainApp().make_app()
+    app = StylerApp().make_app()
+    app.listen(8881)
+    tornado.ioloop.IOLoop.current().start()
---
+++ examples/swapper.py
@@ -0,0 +1,48 @@
+import tornado.ioloop
+from webpy import MainApp
+from webpy import Widget, Button, Input, HBox, VBox, CheckBox, Label
+
+
+class SwapperApp(MainApp):
+
+    def __init__(self):
+        super().__init__()
+
+        self.button = Button("Swap", style={'margin': '3px'})
+        self.secbox = HBox()
+        st = {'width': '300px',
+              'padding': '2px',
+              'border': '1px solid gray',
+              'margin': '3px'}
+        self.left = VBox(style=st)
+        self.right = VBox(style=st)
+
+        self.secbox.children = [
+            self.left, self.right
+        ]
+
+        self.labelA = Label("Label A")
+        self.labelB = Label("label B")
+
+        self.left.children = [self.labelA]
+        self.right.children = [self.labelB]
+
+        self.children = [
+            self.button, self.secbox
+        ]
+
+        self.button.register('click', self.on_swap)
+
+    def on_swap(self, source):
+
+        left = self.left.children[0]
+        right = self.right.children[0]
+
+        self.left.children = [right]
+        self.right.children = [left]
+
+
+if __name__ == "__main__":
+    app = SwapperApp().make_app()
+    app.listen(8881)
+    tornado.ioloop.IOLoop.current().start()
