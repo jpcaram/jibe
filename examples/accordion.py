3237b417f13cb57ec0df9c86195c62d177d59967
Juan Pablo Caram <jpcaram@gmail.com>
2020-02-20 00:43:28 -0500

    Accordion and thirdparty examples working.

---
+++ examples/accordion.py
@@ -0,0 +1,118 @@
+from webpy import Widget, MainApp, Button, Input, \
+    HBox, VBox, Label, ProgressBar
+
+
+class Accordion(Widget):
+    """
+    Example of a complex container widget.
+
+    Its children must be put in self.body.children. This is
+    a bit inconvenient. Perhaps redefine self.children...
+    but that would cause a mess.
+    """
+
+    def __init__(self):
+
+        super().__init__(
+            style={
+                'border': '0',
+                'padding': '0',
+                'margin': '0'
+            }
+        )
+
+        self.titlebar = HBox(
+            style={
+                'border': '1px solid gray',
+                'width': '100%',
+            }
+        )
+        self.titlelabel = Label('Title')
+        self.titlebar.children = [
+            self.titlelabel
+        ]
+        self.titlebar._jshandlers['click'] = """
+            this.message({event: 'click'});
+        """
+        self.titlebar.local_event_handlers['click'] = self.on_bar_click
+
+        self.body = VBox(
+            style={
+                'display': 'none',
+                'border-style': 'solid',
+                'border-color': 'gray',
+                'border-width': '0px 1px 1px 1px',
+                'width': '100%',
+                'overflow': 'hidden'  # Solves missing part of the
+                                      # side borders.
+            }
+        )
+
+        # self.body.children = [
+        #     ProgressBar(),
+        #     Label('Some label')
+        # ]
+
+        self.children = [
+            self.titlebar,
+            self.body,
+            Label('Another label')
+        ]
+
+        # self.titlebar.register('click', self.on_toggle)
+
+        self.renderOnChange = False
+
+        # TODO: This triggers the on_change() callback and
+        #   tries to access self.body, which must exist,
+        #   so this must be placed before it has been
+        #   declared...
+        self.properties['open'] = False
+
+    def on_bar_click(self, msg):
+        print("On Click of titlebar")
+        # self.body.css({'display': 'inherit'})
+        self.open = not self.open
+
+    # def on_toggle(self, source):
+    #     print("on_toggle()!")
+
+    def on_change(self, propname, newval, oldval):
+        super().on_change(propname, newval, oldval)
+        if self.open:
+            self.body.css({'display': 'inherit'})
+        else:
+            self.body.css({'display': 'none'})
+
+    # def on_children_change(self):
+    #     # self.body.update_descendents = lambda *x: None
+    #     self.body.children = [c for c in self._children]
+    #     self._children = []
+    #     # for child in self.children:
+    #     #     child.parent = self  # Invokes child.parent.update_descendents().
+
+    # def update_descendents(self, *args):
+    #     pass
+
+    def on_children_append(self, *args, **kwargs):
+        self.body.children.append(args[0])
+
+
+class ExampleApp(MainApp):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+
+        self.acc = Accordion()
+        self.acc.body.children = [
+            ProgressBar(),
+            Label('Some label')
+        ]
+
+        self.children = [
+            self.acc
+        ]
+
+
+if __name__ == "__main__":
+    ExampleApp.run(port=8881)
---
+++ examples/thirdparty.py
@@ -0,0 +1,88 @@
+from webpy import MainApp, Button, Input, MainHandler, \
+    WebSocketHandler, Label, Widget, VBox
+import tornado.web
+import tornado.ioloop
+from pathlib import Path
+
+
+class GooglePieChart(VBox):
+
+    def __init__(self):
+        super().__init__()
+
+        self._jsrender = f"""
+        gchartprom.done(function() {{
+            console.log("Attempting to render the chart!");
+        
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+              ['Mushrooms', 3],
+              ['Onions', 1],
+              ['Olives', 1],
+              ['Zucchini', 1],
+              ['Pepperoni', 2]
+            ]);
+    
+            // Set chart options
+            var options = {{'title':'How Much Pizza I Ate Last Night',
+                           'width':400,
+                           'height':300}};
+    
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(
+               //document.getElementById('{self.identifier}')
+               this.el
+            );
+            chart.draw(data, options);
+        
+        }}.bind(this));
+        """
+
+
+class ExampleApp(MainApp):
+
+    def __init__(self, connection):
+        super().__init__(connection)
+
+        self.children = [
+            Label('3rd Party Widget'),
+            GooglePieChart()
+        ]
+
+
+class MainHandlerTP(MainHandler):
+
+    def scripts(self):
+        return ["https://www.gstatic.com/charts/loader.js"]
+
+    def presetup(self):
+        return """
+        let gchartprom = $.Deferred();
+        google.charts.load('current', {'packages':['corechart']});
+        function resolvegchart() { gchartprom.resolve(); } 
+        google.charts.setOnLoadCallback(resolvegchart);
+        """
+
+
+class WSH(WebSocketHandler):
+    mainApp = ExampleApp
+
+
+class MultiApp(tornado.web.Application):
+
+    def __init__(self):
+
+        super().__init__([
+            (r"/", MainHandlerTP),
+            (r"/websocket", WSH),
+            (r"/(.*\.js)", tornado.web.StaticFileHandler, {"path": f"{Path(__file__).parent.absolute()}/../webpy/"}),
+            (r"/(.*\.css)", tornado.web.StaticFileHandler, {"path": f"{Path(__file__).parent.absolute()}/../webpy/"})
+        ])
+
+
+if __name__ == "__main__":
+    MultiApp().listen(8881)
+    tornado.ioloop.IOLoop.current().start()
